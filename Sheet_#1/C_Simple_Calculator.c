b31da36a12062bead98d91984fb4bb37eb3530de
Md. Roshiduzzaman ( Ratul ) <roshiduzzaman1913@gmail.com>
2026-01-17 08:55:29 +0600

    Solution for problem C - sheet 1

---
+++ Sheet_#1/C_Simple_Calculator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    long long a, b, sum, mul, sub;
+    scanf("%lld%lld", &a, &b);
+
+    sum = a+b;
+    mul = a*b;
+    sub = a-b;
+
+    printf("%lld + %lld = %lld\n", a, b, sum);
+    printf("%lld * %lld = %lld\n", a, b, mul);
+    printf("%lld - %lld = %lld\n", a, b, sub);
+
+    return 0;
+}
