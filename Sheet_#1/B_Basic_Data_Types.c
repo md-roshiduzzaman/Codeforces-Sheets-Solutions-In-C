83aabd4049545fca8c89f78e66a89baf0728ef12
Md. Roshiduzzaman ( Ratul ) <roshiduzzaman1913@gmail.com>
2026-01-17 08:43:41 +0600

    S

---
+++ Sheet_#1/B_Basic_Data_Types.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+  int num = 0;
+  long long int l = 0;
+  char s;
+  float f;
+  double d;
+  scanf("%d %lld %c %f %lf", &num, &l, &s, &f, &d);
+  printf("%d\n", num);
+  printf("%lld\n", l);
+  printf("%c\n", s);
+  printf("%.2f\n", f);
+  printf("%.1lf", d);
+
+  return 0;
+}
