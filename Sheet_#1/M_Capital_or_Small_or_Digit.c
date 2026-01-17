4d75b6cf49301113ff89fd60fd3bee296e139d7b
Md. Roshiduzzaman ( Ratul ) <roshiduzzaman1913@gmail.com>
2026-01-17 17:14:25 +0600

    Solution for problem M - sheet 1

---
+++ Sheet_#1/M_Capital_or_Small_or_Digit.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c", &ch);
+    
+    if(ch>='A' && ch<='Z')
+    {
+        printf("ALPHA\nIS CAPITAL\n");
+    }
+    else if(ch>='a' && ch<='z')
+    {
+        printf("ALPHA\nIS SMALL");
+    }
+    else if(ch>='0' && ch<='9')
+    {
+        printf("IS DIGIT\n");
+    }
+
+    return 0;
+}
