30be41543b9bbb48392c51039f5f57f99625b64d
Md. Roshiduzzaman ( Ratul ) <roshiduzzaman1913@gmail.com>
2026-01-20 00:11:14 +0600

    Solution for problem T - sheet 2

---
+++ Sheet_#2/T_Shape2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=n-i; j>=1; j--)
+        {
+            printf(" ");
+        }
+        for(int k=1; k<=i*2-1; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
