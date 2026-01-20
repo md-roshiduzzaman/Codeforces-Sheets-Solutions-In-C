4b1cf75c13700f934f12a022624c6049b9cacdcd
Md. Roshiduzzaman ( Ratul ) <roshiduzzaman1913@gmail.com>
2026-01-20 08:05:19 +0600

    Solution for problem D - sheet 3

---
+++ Sheet_#3/D_Positions_in_array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    long long n;
+    scanf("%lld", &n);
+    long long arr[n];
+
+    for(int i=0; i<n; i++)
+    {
+        scanf("%lld", &arr[i]);
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i] <= 10)
+        {
+            printf("A[%d] = %lld\n", i, arr[i]);
+        }
+    }
+
+    return 0;
+}
