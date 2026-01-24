8c20b81daa81b807a15ed58385feaa47b57a930d
Md. Roshiduzzaman ( Ratul ) <roshiduzzaman1913@gmail.com>
2026-01-24 10:29:47 +0600

    Solution for problem Y - sheet 2

---
+++ Sheet_#2/Y_Easy_Fibonacci.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n, first=0, second=1, next;
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++)
+    {
+        printf("%d ", first);
+
+        next = first+second;
+        first = second;
+        second = next;
+    }
+
+    return 0;
+}
