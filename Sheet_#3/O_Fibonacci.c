f521588781063a867a305cdef9244c5f0d14c07b
Md. Roshiduzzaman ( Ratul ) <roshiduzzaman1913@gmail.com>
2026-01-26 09:05:44 +0600

    Solution for problem O - sheet 3

---
+++ Sheet_#3/O_Fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+
+    long long fib[50];
+    fib[1] = 0;
+    fib[2] = 1;
+
+    for (i = 3; i <= n; i++)
+    {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    printf("%lld\n", fib[n]);
+
+    return 0;
+}
