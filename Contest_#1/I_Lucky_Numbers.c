991a6971a7fcf22faa7c8186379f2f23d7a36e7f
Md. Roshiduzzaman ( Ratul ) <roshiduzzaman1913@gmail.com>
2026-02-05 00:32:04 +0600

    Solution for problem I - contest 1

---
+++ Contest_#1/I_Lucky_Numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n, a, b;
+    scanf("%d", &n);
+
+    a = n%10;
+    b = n/10;
+
+    if(a%b==0 || b%a==0)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+    
+    return 0;
+}
