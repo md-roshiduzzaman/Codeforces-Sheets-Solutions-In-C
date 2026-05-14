70c6b77b20e97c022e648a15a9a963897d82b04c
Md. Roshiduzzaman ( Ratul ) <roshiduzzaman1913@gmail.com>
2026-05-14 23:11:23 +0600

    Solution for B, C++

---
+++ Sheet_#5/B_Print.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void print(int &n)
+{
+    for (int i = 1; i <= n; i++) 
+    {
+        cout << i;
+        if (i != n) cout << " ";
+    }
+}
+ 
+int main()
+{
+    int N;
+    cin >> N;
+    print(N);
+    return 0;
+}
